cf7a241f43f9f3b6da47adf066a7724cabf92bc9
R.J. Davis <rjdavis@channelape.com>
2018-04-27 11:20:48 -0400

    CA-3376 Added action update to Actions Service.

--- src/actions/service/ActionsService.ts
+++ src/actions/service/ActionsService.ts
@@ -3,6 +3,7 @@
 import  Action from '../model/Action';
 import request = require('request');
 import Resource from '../../model/Resource';
+import Subresource from '../model/Subresource';
 import Version from '../../model/Version';
 import ChannelApeErrorResponse from './../../model/ChannelApeErrorResponse';
 import * as Q from 'q';
@@ -11,10 +12,10 @@ export default class ActionsService {
   constructor(private readonly client: request.RequestAPI<request.Request, 
     request.CoreOptions, request.RequiredUriUrl>) { }
 
-  public get(sessionId: string, id: string): Q.Promise<Action> {
+  public get(sessionId: string, actionId: string): Q.Promise<Action> {
 
     const deferred = Q.defer<Action>();
-    const requestUrl = `/${Version.V1}${Resource.ACTIONS}/${id}`;
+    const requestUrl = `/${Version.V1}${Resource.ACTIONS}/${actionId}`;
 
     const options : request.CoreOptions = {
       headers: {
@@ -22,17 +23,69 @@ export default class ActionsService {
       }
     };
     this.client.get(requestUrl, options, (error, response, body) => {
-      if (error) {
-        deferred.reject(error);
-      } else if (response.statusCode === 200) {
-        deferred.resolve(body as Action);
-      } else {
-        const channelApeErrorResponse = body as ChannelApeErrorResponse;
-        channelApeErrorResponse.statusCode = response.statusCode;
-        deferred.reject(channelApeErrorResponse);
-      }
+      this.mapPromise(deferred, error, response, body);
     });
 
     return deferred.promise;
+  }
+
+  public updateHealthCheck(sessionId: string, actionId: string): Q.Promise<Action> {
+    const requestUrl = `/${Version.V1}${Resource.ACTIONS}/${actionId}/${Subresource.HEALTH_CHECK}`;
+    
+    const options : request.CoreOptions = {
+      headers: {
+        'X-Channel-Ape-Authorization-Token' : sessionId
+      }
+    };
+
+    const deferred = Q.defer<Action>();
+    this.client.put(requestUrl, options, (error, response, body) => {
+      this.mapPromise(deferred, error, response, body);
+    });
+    return deferred.promise;
+  }
+
+  public complete(sessionId: string, actionId: string): Q.Promise<Action> {
+    const requestUrl = `/${Version.V1}${Resource.ACTIONS}/${actionId}/${Subresource.COMPLETE}`;
+    
+    const options : request.CoreOptions = {
+      headers: {
+        'X-Channel-Ape-Authorization-Token' : sessionId
+      }
+    };
+
+    const deferred = Q.defer<Action>();
+    this.client.put(requestUrl, options, (error, response, body) => {
+      this.mapPromise(deferred, error, response, body);
+    });
+    return deferred.promise;
+  }
+
+  public error(sessionId: string, actionId: string): Q.Promise<Action> {
+    const requestUrl = `/${Version.V1}${Resource.ACTIONS}/${actionId}/${Subresource.ERROR}`;
+    
+    const options : request.CoreOptions = {
+      headers: {
+        'X-Channel-Ape-Authorization-Token' : sessionId
+      }
+    };
+
+    const deferred = Q.defer<Action>();
+    this.client.put(requestUrl, options, (error, response, body) => {
+      this.mapPromise(deferred, error, response, body);
+    });
+    return deferred.promise;
+  }
+
+  private mapPromise(deferred: Q.Deferred<Action>, error: any, response: request.Response, body : any) {
+    if (error) {
+      deferred.reject(error);
+    } else if (response.statusCode === 200) {
+      deferred.resolve(body as Action);
+    } else {
+      const channelApeErrorResponse = body as ChannelApeErrorResponse;
+      channelApeErrorResponse.statusCode = response.statusCode;
+      deferred.reject(channelApeErrorResponse);
+    }
   }
 }
--- test/actions/service/ActionsService.spec.ts
+++ test/actions/service/ActionsService.spec.ts
@@ -4,6 +4,7 @@ import request = require('request');
 import ActionsService from './../../../src/actions/service/ActionsService';
 import Version from '../../../src/model/Version';
 import Resource from '../../../src/model/Resource';
+import Subresource from '../../../src/actions/model/Subresource';
 import Environment from '../../../src/model/Environment';
 import ChannelApeErrorResponse from '../../../src/model/ChannelApeErrorResponse';
 import Action from '../../../src/actions/model/Action';
@@ -34,6 +35,20 @@ describe('Actions Service', () => {
       targetType: 'supplier'
     };
 
+    const expectedChannelApeErrorResponse : ChannelApeErrorResponse = {
+      statusCode: 404,
+      errors: [
+        { 
+          code: 111, 
+          message: 'Action could not be found.' 
+        }
+      ]
+    };
+
+    const expectedError = {
+      stack: 'oh no an error'
+    };
+
     beforeEach((done) => {
       sandbox = sinon.sandbox.create();
       done();
@@ -57,32 +72,14 @@ describe('Actions Service', () => {
       return actionsService.get(expectedSessionId, expectedAction.id).then((actualAction) => {
         expect(clientGetStub.args[0][0]).to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}`);
         const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
-        const actualHeaders = actualOptions.headers;
-        if (actualHeaders == null) {
-          expect(actualHeaders).to.not.be.undefined;
-        } else {
-          expect(actualHeaders['X-Channel-Ape-Authorization-Token']).to.equal(expectedSessionId);
-        }
-
-        expect(actualAction.action).to.equal(expectedAction.action);
-        expect(actualAction.businessId).to.equal(expectedAction.businessId);
-        expect(actualAction.description).to.equal(expectedAction.description);
-        expect(actualAction.healthCheckIntervalInSeconds).to.equal(expectedAction.healthCheckIntervalInSeconds);
-        expect(actualAction.id).to.equal(expectedAction.id);
-        expect(actualAction.lastHealthCheckTime).to.equal(expectedAction.lastHealthCheckTime);
-        expect(actualAction.processingStatus).to.equal(expectedAction.processingStatus);
-        expect(actualAction.startTime).to.equal(expectedAction.startTime);
-        expect(actualAction.targetId).to.equal(expectedAction.targetId);
-        expect(actualAction.targetType).to.equal(expectedAction.targetType);
+        expectOptions(actualOptions);
+        expectAction(expectedAction);
       });
     });
 
     it('And valid session ID And valid action ID And request connect errors ' +
       'When retrieving action Then return a rejected promise with an error', () => {
 
-      const expectedError = {
-        stack: 'oh no an error'
-      };
       const clientGetStub = sandbox.stub(client, 'get')
         .yields(expectedError, null, null);
 
@@ -92,14 +89,8 @@ describe('Actions Service', () => {
       }).catch((e) => {
         expect(clientGetStub.args[0][0]).to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}`);
         const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
-        const actualHeaders = actualOptions.headers;
-        if (actualHeaders == null) {
-          expect(actualHeaders).to.not.be.undefined;
-        } else {
-          expect(actualHeaders['X-Channel-Ape-Authorization-Token']).to.equal(expectedSessionId);
-        }
-
-        expect(e).to.be.equal(expectedError);
+        expectOptions(actualOptions);
+        expect(e).to.equal(expectedError);
       });
     });
 
@@ -108,16 +99,7 @@ describe('Actions Service', () => {
     'And action not found error message', () => {
 
       const response = {
-        statusCode: 401
-      };
-      const expectedChannelApeErrorResponse : ChannelApeErrorResponse = {
-        statusCode: 401,
-        errors: [
-          { 
-            code: 12, 
-            message: 'Invalid authorization token. Please check the server logs and try again.' 
-          }
-        ]
+        statusCode: 404
       };
       const clientGetStub = sandbox.stub(client, 'get')
         .yields(null, response, expectedChannelApeErrorResponse);
@@ -128,20 +110,229 @@ describe('Actions Service', () => {
       }).catch((e) => {
         expect(clientGetStub.args[0][0]).to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}`);
         const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
-        const actualHeaders = actualOptions.headers;
-        if (actualHeaders == null) {
-          expect(actualHeaders).to.not.be.undefined;
-        } else {
-          expect(actualHeaders['X-Channel-Ape-Authorization-Token']).to.equal(expectedSessionId);
-        }
-
-        const actualChannelApeErrorResponse = e as ChannelApeErrorResponse;
-        expect(actualChannelApeErrorResponse.statusCode).to.equal(401);
-        expect(actualChannelApeErrorResponse.errors[0].code).to.equal(expectedChannelApeErrorResponse.errors[0].code);
-        expect(actualChannelApeErrorResponse.errors[0].message)
-          .to.equal(expectedChannelApeErrorResponse.errors[0].message);
+        expectOptions(actualOptions);
+        expectChannelApeErrorResponse(e);
       });
     });
+
+    it('And valid session ID And valid action ID ' +
+    'When updating action health check Then return resolved promise with action', () => {
+
+      const response = {
+        statusCode: 200
+      };
+      const clientGetStub: sinon.SinonStub = sandbox.stub(client, 'put')
+          .yields(null, response, expectedAction);
+
+      const actionsService: ActionsService = new ActionsService(client);
+      return actionsService.updateHealthCheck(expectedSessionId, expectedAction.id).then((actualAction) => {
+        expect(clientGetStub.args[0][0])
+          .to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}/${Subresource.HEALTH_CHECK}`);
+        const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
+        expectOptions(actualOptions);
+        expectAction(expectedAction);
+      });
+    });
+
+    it('And valid session ID And valid action ID And request connect errors ' +
+    'When updating action health check Then return a rejected promise with an error', () => {
+
+      const expectedError = {
+        stack: 'oh no an error'
+      };
+      const clientGetStub = sandbox.stub(client, 'put')
+        .yields(expectedError, null, null);
+
+      const actionsService: ActionsService = new ActionsService(client);
+      return actionsService.updateHealthCheck(expectedSessionId, expectedAction.id).then((actualResponse) => {
+        expect(actualResponse).to.be.undefined;
+      }).catch((e) => {
+        expect(clientGetStub.args[0][0])
+          .to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}/${Subresource.HEALTH_CHECK}`);
+        const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
+        expectOptions(actualOptions);
+        expect(e).to.equal(expectedError);
+      });
+    });
+
+    it('And valid session ID And invalid action ID ' +
+    'When updating action health check Then return a rejected promise with 404 status code ' +
+    'And action not found error message', () => {
+
+      const response = {
+        statusCode: 404
+      };
+
+      const clientGetStub = sandbox.stub(client, 'put')
+        .yields(null, response, expectedChannelApeErrorResponse);
+
+      const actionsService: ActionsService = new ActionsService(client);
+      return actionsService.updateHealthCheck(expectedSessionId, expectedAction.id).then((actualResponse) => {
+        expect(actualResponse).to.be.undefined;
+      }).catch((e) => {
+        expect(clientGetStub.args[0][0])
+          .to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}/${Subresource.HEALTH_CHECK}`);
+        const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
+        expectOptions(actualOptions);
+        expectChannelApeErrorResponse(e);
+      });
+    });
+
+    it('And valid session ID And valid action ID ' +
+    'When completing action Then return resolved promise with action', () => {
+
+      const response = {
+        statusCode: 200
+      };
+      const clientGetStub: sinon.SinonStub = sandbox.stub(client, 'put')
+          .yields(null, response, expectedAction);
+
+      const actionsService: ActionsService = new ActionsService(client);
+      return actionsService.complete(expectedSessionId, expectedAction.id).then((actualAction) => {
+        expect(clientGetStub.args[0][0])
+          .to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}/${Subresource.COMPLETE}`);
+        const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
+        expectOptions(actualOptions);
+        expectAction(expectedAction);
+      });
+    });
+
+    it('And valid session ID And valid action ID And request connect errors ' +
+    'When completing action Then return a rejected promise with an error', () => {
+
+      const expectedError = {
+        stack: 'oh no an error'
+      };
+      const clientGetStub = sandbox.stub(client, 'put')
+        .yields(expectedError, null, null);
+
+      const actionsService: ActionsService = new ActionsService(client);
+      return actionsService.complete(expectedSessionId, expectedAction.id).then((actualResponse) => {
+        expect(actualResponse).to.be.undefined;
+      }).catch((e) => {
+        expect(clientGetStub.args[0][0])
+          .to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}/${Subresource.COMPLETE}`);
+        const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
+        expectOptions(actualOptions);
+        expect(e).to.equal(expectedError);
+      });
+    });
+
+    it('And valid session ID And invalid action ID ' +
+    'When completing action Then return a rejected promise with 404 status code ' +
+    'And action not found error message', () => {
+
+      const response = {
+        statusCode: 404
+      };
+
+      const clientGetStub = sandbox.stub(client, 'put')
+        .yields(null, response, expectedChannelApeErrorResponse);
+
+      const actionsService: ActionsService = new ActionsService(client);
+      return actionsService.complete(expectedSessionId, expectedAction.id).then((actualResponse) => {
+        expect(actualResponse).to.be.undefined;
+      }).catch((e) => {
+        expect(clientGetStub.args[0][0])
+          .to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}/${Subresource.COMPLETE}`);
+        const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
+        expectOptions(actualOptions);
+        expectChannelApeErrorResponse(e);
+      });
+    });
+
+    it('And valid session ID And valid action ID ' +
+    'When updating action with error Then return resolved promise with action', () => {
+
+      const response = {
+        statusCode: 200
+      };
+      const clientGetStub: sinon.SinonStub = sandbox.stub(client, 'put')
+          .yields(null, response, expectedAction);
+
+      const actionsService: ActionsService = new ActionsService(client);
+      return actionsService.error(expectedSessionId, expectedAction.id).then((actualAction) => {
+        expect(clientGetStub.args[0][0])
+          .to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}/${Subresource.ERROR}`);
+        const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
+        expectOptions(actualOptions);
+        expectAction(expectedAction);
+      });
+    });
+
+    it('And valid session ID And valid action ID And request connect errors ' +
+    'When updating action with error Then return a rejected promise with an error', () => {
+
+      const expectedError = {
+        stack: 'oh no an error'
+      };
+      const clientGetStub = sandbox.stub(client, 'put')
+        .yields(expectedError, null, null);
+
+      const actionsService: ActionsService = new ActionsService(client);
+      return actionsService.error(expectedSessionId, expectedAction.id).then((actualResponse) => {
+        expect(actualResponse).to.be.undefined;
+      }).catch((e) => {
+        expect(clientGetStub.args[0][0])
+          .to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}/${Subresource.ERROR}`);
+        const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
+        expectOptions(actualOptions);
+        expect(e).to.equal(expectedError);
+      });
+    });
+
+    it('And valid session ID And invalid action ID ' +
+    'When updating action with error Then return a rejected promise with 404 status code ' +
+    'And action not found error message', () => {
+
+      const response = {
+        statusCode: 404
+      };
+
+      const clientGetStub = sandbox.stub(client, 'put')
+        .yields(null, response, expectedChannelApeErrorResponse);
+
+      const actionsService: ActionsService = new ActionsService(client);
+      return actionsService.error(expectedSessionId, expectedAction.id).then((actualResponse) => {
+        expect(actualResponse).to.be.undefined;
+      }).catch((e) => {
+        expect(clientGetStub.args[0][0])
+          .to.equal(`/${Version.V1}${Resource.ACTIONS}/${expectedAction.id}/${Subresource.ERROR}`);
+        const actualOptions: request.CoreOptions = clientGetStub.args[0][1];
+        expectOptions(actualOptions);
+        expectChannelApeErrorResponse(e);
+      });
+    });
+
+    function expectOptions(actualOptions: request.CoreOptions) {
+      const actualHeaders = actualOptions.headers;
+      if (actualHeaders == null) {
+        expect(actualHeaders).to.not.be.undefined;
+      } else {
+        expect(actualHeaders['X-Channel-Ape-Authorization-Token']).to.equal(expectedSessionId);
+      }
+    }
+
+    function expectAction(actualAction: Action) {
+      expect(actualAction.action).to.equal(expectedAction.action);
+      expect(actualAction.businessId).to.equal(expectedAction.businessId);
+      expect(actualAction.description).to.equal(expectedAction.description);
+      expect(actualAction.healthCheckIntervalInSeconds).to.equal(expectedAction.healthCheckIntervalInSeconds);
+      expect(actualAction.id).to.equal(expectedAction.id);
+      expect(actualAction.lastHealthCheckTime).to.equal(expectedAction.lastHealthCheckTime);
+      expect(actualAction.processingStatus).to.equal(expectedAction.processingStatus);
+      expect(actualAction.startTime).to.equal(expectedAction.startTime);
+      expect(actualAction.targetId).to.equal(expectedAction.targetId);
+      expect(actualAction.targetType).to.equal(expectedAction.targetType);
+    }
+
+    function expectChannelApeErrorResponse(error: any) {
+      const actualChannelApeErrorResponse = error as ChannelApeErrorResponse;
+      expect(actualChannelApeErrorResponse.statusCode).to.equal(404);
+      expect(actualChannelApeErrorResponse.errors[0].code).to.equal(expectedChannelApeErrorResponse.errors[0].code);
+      expect(actualChannelApeErrorResponse.errors[0].message)
+        .to.equal(expectedChannelApeErrorResponse.errors[0].message);
+    }
 
   });
 });
