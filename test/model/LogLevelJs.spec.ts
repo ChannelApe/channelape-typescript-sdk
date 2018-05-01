132ebde3d3fe5fde44d95f8996e164f7195dc07a
Craig Simko <csimko@channelape.com>
2018-05-01 09:09:59 -0400

    CA-3380: Added new spec to test javascript implementation for log level when creating the channel ape client.

--- test/model/LogLevelJs.spec.ts
+++ test/model/LogLevelJs.spec.ts
@@ -1,17 +1,17 @@
 const LogLevel = require('../../src/model/LogLevel').LogLevel;
 const getLogLevelName = require('../../src/model/LogLevel').getLogLevelName;
-const chai = require('chai');
+const expect = require('chai').expect;
 
 describe('LogLevel JS', () => {
   describe('getLogLevelName()', () => {
     describe('Given null as a parameter', () => {
       it('Then expect "OFF" to be returned', () => {
-        chai.expect(getLogLevelName()).to.equal('OFF');
+        expect(getLogLevelName()).to.equal('OFF');
       });
     });
     describe('Given an empty string as a parameter', () => {
       it('Then expect "OFF" to be returned', () => {
-        chai.expect(getLogLevelName('')).to.equal('OFF');
+        expect(getLogLevelName('')).to.equal('OFF');
       });
     });
   });
