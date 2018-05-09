7f1c1244fb9f407e94925a37b50dd91b1528cd2c
Craig Simko <csimko@channelape.com>
2018-05-09 11:22:57 -0400

    CA-3380: Properly stubbed API calls in the Request Wrapper

--- test/RequestClientWrapper.spec.ts
+++ test/RequestClientWrapper.spec.ts
@@ -6,6 +6,8 @@ import RequestClientWrapper from '../src/RequestClientWrapper';
 
 import singleOrder from './orders/resources/singleOrder';
 import singleOrderToUpdate from './orders/resources/singleOrderToUpdate';
+import multipleOrders from './orders/resources/multipleOrders';
+import ChannelApeError from '../src/model/ChannelApeError';
 
 describe('RequestClientWrapper', () => {
 
@@ -23,17 +25,16 @@ describe('RequestClientWrapper', () => {
       done();
     });
 
-    const requestClientWrapper: RequestClientWrapper =
-      new RequestClientWrapper(
-        request.defaults({
-          baseUrl: Environment.STAGING,
-          timeout: 60000,
-          json: true,
-          headers: {
-            'X-Channel-Ape-Authorization-Token': 'valid-session-id'
-          }
-        })
-      );
+    const client = request.defaults({
+      baseUrl: Environment.STAGING,
+      timeout: 60000,
+      json: true,
+      headers: {
+        'X-Channel-Ape-Authorization-Token': 'valid-session-id'
+      }
+    });
+
+    const requestClientWrapper: RequestClientWrapper = new RequestClientWrapper(client);
 
     it('When doing a get() with just a URI and call back expect data to be returned', (done) => {
       const orderId = 'c0f45529-cbed-4e90-9a38-c208d409ef2a';
@@ -41,7 +42,9 @@ describe('RequestClientWrapper', () => {
       const response = {
         statusCode: 200
       };
-      
+      const clientGetStub: sinon.SinonStub = sandbox.stub(client, 'get')
+        .yields(null, response, singleOrder);
+
       requestClientWrapper.get(requestUrl, (error, response, body) => {
         expect(error).to.be.null;
         expect(body.id).to.equal(orderId);
@@ -59,6 +62,12 @@ describe('RequestClientWrapper', () => {
           status: 'OPEN'
         }
       };
+      const response = {
+        statusCode: 200
+      };
+      const clientGetStub: sinon.SinonStub = sandbox.stub(client, 'get')
+        .yields(null, response, { orders: multipleOrders });
+
       requestClientWrapper.get(requestUrl, options, (error, response, body) => {
         expect(error).to.be.null;
         expect(body.orders[0].businessId).to.equal(businessId);
@@ -77,6 +86,12 @@ describe('RequestClientWrapper', () => {
           status: 'OPEN'
         }
       };
+      const response = {
+        statusCode: 200
+      };
+      const clientGetStub: sinon.SinonStub = sandbox.stub(client, 'get')
+        .yields(null, response, { orders: multipleOrders });
+
       requestClientWrapper.get(options, (error, response, body) => {
         expect(error).to.be.null;
         expect(body.orders[0].businessId).to.equal(businessId);
@@ -102,6 +117,16 @@ describe('RequestClientWrapper', () => {
     it('When doing a put() with just a URI and call back expect ChannelApe error to be returned', (done) => {
       const orderId = 'c0f45529-cbed-4e90-9a38-c208d409ef2a';
       const requestUrl = `/v1/orders/${orderId}`;
+      const response = {
+        statusCode: 404
+      };
+      const channelApeError: ChannelApeError = {
+        code: 0,
+        message: 'You didnt pass any body'
+      };
+      const clientGetStub: sinon.SinonStub = sandbox.stub(client, 'put')
+        .yields(null, response, { errors: [channelApeError] });
+
       requestClientWrapper.put(requestUrl, (error, response, body) => {
         expect(error).to.be.null;
         expect(body.errors[0].code).to.equal(0);
@@ -117,6 +142,12 @@ describe('RequestClientWrapper', () => {
       const options: request.CoreOptions = {
         body: singleOrderToUpdate
       };
+      const response = {
+        statusCode: 202
+      };
+      const clientGetStub: sinon.SinonStub = sandbox.stub(client, 'put')
+        .yields(null, response, singleOrderToUpdate);
+
       requestClientWrapper.put(requestUrl, options, (error, response, body) => {
         expect(error).to.be.null;
         expect(body.additionalFields[0].value).to.equal('RRR');
@@ -133,6 +164,12 @@ describe('RequestClientWrapper', () => {
         uri: requestUrl,
         body: singleOrderToUpdate
       };
+      const response = {
+        statusCode: 202
+      };
+      const clientGetStub: sinon.SinonStub = sandbox.stub(client, 'put')
+        .yields(null, response, singleOrderToUpdate);
+
       requestClientWrapper.put(options, (error, response, body) => {
         expect(error).to.be.null;
         expect(body.additionalFields[0].value).to.equal('RRR');
