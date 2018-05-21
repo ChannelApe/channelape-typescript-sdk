cace4efe3cc8d2d7a3e83647bf9f2a01aa05cf8d
Craig Simko <csimko@channelape.com>
2018-05-21 11:30:11 -0400

    Updated log level enum to be more in line with what Winston expects.

--- src/model/LogLevel.ts
+++ src/model/LogLevel.ts
@@ -1,17 +1,10 @@
 export enum LogLevel {
-  OFF = -1,
-  ERROR = 0,
-  WARN = 1,
-  INFO = 2,
-  VERBOSE = 3,
-  DEBUG = 4
-}
-
-export function getLogLevelName(level: LogLevel): string {
-  if (!LogLevel.hasOwnProperty(level)) {
-    return 'OFF';
-  }
-  return LogLevel[level];
+  OFF = 'off',
+  ERROR = 'error',
+  WARN = 'warn',
+  INFO = 'info',
+  VERBOSE = 'verbose',
+  DEBUG = 'debug'
 }
 
 export default LogLevel;
