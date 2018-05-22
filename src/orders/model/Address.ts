9f55204928841a86688e3b67e610c5f4bad12963
Craig Simko <csimko@channelape.com>
2018-05-22 16:45:31 -0400

    Made every address property optional.

--- src/orders/model/Address.ts
+++ src/orders/model/Address.ts
@@ -1,17 +1,17 @@
 import AdditionalField from '../../model/AdditionalField';
 
 export default interface Address {
-  additionalFields: AdditionalField[];
-  address1: string;
+  additionalFields?: AdditionalField[];
+  address1?: string;
   address2?: string;
-  city: string;
-  country: string;
-  countryCode: string;
-  firstName: string;
-  lastName: string;
-  name: string;
-  postalCode: string;
-  provinceCode: string;
-  province: string;
+  city?: string;
+  country?: string;
+  countryCode?: string;
+  firstName?: string;
+  lastName?: string;
+  name?: string;
+  postalCode?: string;
+  provinceCode?: string;
+  province?: string;
   phone?: string;
 }
