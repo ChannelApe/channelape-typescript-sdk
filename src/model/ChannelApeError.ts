626bfee845c1cb7e81f6d123691090be33214dbf
Craig Simko <csimko@channelape.com>
2018-05-31 11:26:52 -0400

    CA-3581: Refactored ChannelApeError constructor.

--- src/model/ChannelApeError.ts
+++ src/model/ChannelApeError.ts
@@ -8,29 +8,33 @@ export default class ChannelApeError extends Error {
     uri: string,
     apiErrors: ChannelApeApiError[]
   ) {
-    let responseMessage = message;
-    if (apiErrors.length > 0) {
-      const messageParts = apiErrors.map(getApiError).join('\n');
-      responseMessage += `\n${messageParts}`;
-    }
-    let method: string;
-    let statusCode: number;
-    let statusMessage: string;
-    if (typeof response === 'undefined') {
-      method = '';
-      statusCode = 0;
-      statusMessage = '';
-    } else {
-      method = typeof response.method === 'undefined' ? '' : response.method;
-      statusCode = typeof response.statusCode === 'undefined' ? 0 : response.statusCode;
-      statusMessage = typeof response.statusMessage === 'undefined' ? '' : response.statusMessage;
-    }
-    const finalMessage =
+    super(getMessage());
+
+    function getMessage(): string {
+      let ret = message;
+      if (apiErrors.length > 0) {
+        const messageParts = apiErrors.map(getApiError).join('\n');
+        ret += `\n${messageParts}`;
+      }
+      let method: string;
+      let statusCode: number;
+      let statusMessage: string;
+      if (typeof response === 'undefined') {
+        method = '';
+        statusCode = 0;
+        statusMessage = '';
+      } else {
+        method = typeof response.method === 'undefined' ? '' : response.method;
+        statusCode = typeof response.statusCode === 'undefined' ? 0 : response.statusCode;
+        statusMessage = typeof response.statusMessage === 'undefined' ? '' : response.statusMessage;
+      }
+      const finalMessage =
 `${method} ${uri}
   Status: ${statusCode} ${statusMessage}
   Response Body:
-  ${responseMessage}`;
-    super(finalMessage);
+  ${ret}`;
+      return finalMessage;
+    }
 
     function getApiError(apiError: ChannelApeApiError, index: number): string {
       return `Code: ${apiError.code} Message: ${apiError.message}`;
