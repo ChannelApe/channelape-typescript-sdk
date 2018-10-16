713b41a74854edc2672ab4bd78b551d5e05c8668
Craig Simko <csimko@channelape.com>
2018-10-16 10:59:08 -0400

    Adding AlphabeticCurrencyCode enum

---
+++ test/model/AlphabeticCurrencyCode.spec.ts
@@ -0,0 +1,239 @@
+import AlphabeticCurrencyCode from '../../src/model/AlphabeticCurrencyCode';
+import { expect } from 'chai';
+
+describe('AlphabeticCurrencyCode', () => {
+  it('USD', () => {
+    expect(AlphabeticCurrencyCode.USD).to.equal('USD', 'AlphabeticCurrencyCode should contain "USD"');
+  });
+  it('JPY', () => {
+    expect(AlphabeticCurrencyCode.JPY).to.equal('JPY', 'AlphabeticCurrencyCode should contain "JPY"');
+  });
+  it('CNY', () => {
+    expect(AlphabeticCurrencyCode.CNY).to.equal('CNY', 'AlphabeticCurrencyCode should contain "CNY"');
+  });
+  it('SDG', () => {
+    expect(AlphabeticCurrencyCode.SDG).to.equal('SDG', 'AlphabeticCurrencyCode should contain "SDG"');
+  });
+  it('RON', () => {
+    expect(AlphabeticCurrencyCode.RON).to.equal('RON', 'AlphabeticCurrencyCode should contain "RON"');
+  });
+  it('MKD', () => {
+    expect(AlphabeticCurrencyCode.MKD).to.equal('MKD', 'AlphabeticCurrencyCode should contain "MKD"');
+  });
+  it('MXN', () => {
+    expect(AlphabeticCurrencyCode.MXN).to.equal('MXN', 'AlphabeticCurrencyCode should contain "MXN"');
+  });
+  it('CAD', () => {
+    expect(AlphabeticCurrencyCode.CAD).to.equal('CAD', 'AlphabeticCurrencyCode should contain "CAD"');
+  });
+  it('ZAR', () => {
+    expect(AlphabeticCurrencyCode.ZAR).to.equal('ZAR', 'AlphabeticCurrencyCode should contain "ZAR"');
+  });
+  it('AUD', () => {
+    expect(AlphabeticCurrencyCode.AUD).to.equal('AUD', 'AlphabeticCurrencyCode should contain "AUD"');
+  });
+  it('NOK', () => {
+    expect(AlphabeticCurrencyCode.NOK).to.equal('NOK', 'AlphabeticCurrencyCode should contain "NOK"');
+  });
+  it('ILS', () => {
+    expect(AlphabeticCurrencyCode.ILS).to.equal('ILS', 'AlphabeticCurrencyCode should contain "ILS"');
+  });
+  it('ISK', () => {
+    expect(AlphabeticCurrencyCode.ISK).to.equal('ISK', 'AlphabeticCurrencyCode should contain "ISK"');
+  });
+  it('SYP', () => {
+    expect(AlphabeticCurrencyCode.SYP).to.equal('SYP', 'AlphabeticCurrencyCode should contain "SYP"');
+  });
+  it('LYD', () => {
+    expect(AlphabeticCurrencyCode.LYD).to.equal('LYD', 'AlphabeticCurrencyCode should contain "LYD"');
+  });
+  it('UYU', () => {
+    expect(AlphabeticCurrencyCode.UYU).to.equal('UYU', 'AlphabeticCurrencyCode should contain "UYU"');
+  });
+  it('YER', () => {
+    expect(AlphabeticCurrencyCode.YER).to.equal('YER', 'AlphabeticCurrencyCode should contain "YER"');
+  });
+  it('CSD', () => {
+    expect(AlphabeticCurrencyCode.CSD).to.equal('CSD', 'AlphabeticCurrencyCode should contain "CSD"');
+  });
+  it('EEK', () => {
+    expect(AlphabeticCurrencyCode.EEK).to.equal('EEK', 'AlphabeticCurrencyCode should contain "EEK"');
+  });
+  it('THB', () => {
+    expect(AlphabeticCurrencyCode.THB).to.equal('THB', 'AlphabeticCurrencyCode should contain "THB"');
+  });
+  it('IDR', () => {
+    expect(AlphabeticCurrencyCode.IDR).to.equal('IDR', 'AlphabeticCurrencyCode should contain "IDR"');
+  });
+  it('LBP', () => {
+    expect(AlphabeticCurrencyCode.LBP).to.equal('LBP', 'AlphabeticCurrencyCode should contain "LBP"');
+  });
+  it('AED', () => {
+    expect(AlphabeticCurrencyCode.AED).to.equal('AED', 'AlphabeticCurrencyCode should contain "AED"');
+  });
+  it('BOB', () => {
+    expect(AlphabeticCurrencyCode.BOB).to.equal('BOB', 'AlphabeticCurrencyCode should contain "BOB"');
+  });
+  it('QAR', () => {
+    expect(AlphabeticCurrencyCode.QAR).to.equal('QAR', 'AlphabeticCurrencyCode should contain "QAR"');
+  });
+  it('BHD', () => {
+    expect(AlphabeticCurrencyCode.BHD).to.equal('BHD', 'AlphabeticCurrencyCode should contain "BHD"');
+  });
+  it('HNL', () => {
+    expect(AlphabeticCurrencyCode.HNL).to.equal('HNL', 'AlphabeticCurrencyCode should contain "HNL"');
+  });
+  it('HRK', () => {
+    expect(AlphabeticCurrencyCode.HRK).to.equal('HRK', 'AlphabeticCurrencyCode should contain "HRK"');
+  });
+  it('COP', () => {
+    expect(AlphabeticCurrencyCode.COP).to.equal('COP', 'AlphabeticCurrencyCode should contain "COP"');
+  });
+  it('ALL', () => {
+    expect(AlphabeticCurrencyCode.ALL).to.equal('ALL', 'AlphabeticCurrencyCode should contain "ALL"');
+  });
+  it('DKK', () => {
+    expect(AlphabeticCurrencyCode.DKK).to.equal('DKK', 'AlphabeticCurrencyCode should contain "DKK"');
+  });
+  it('MYR', () => {
+    expect(AlphabeticCurrencyCode.MYR).to.equal('MYR', 'AlphabeticCurrencyCode should contain "MYR"');
+  });
+  it('SEK', () => {
+    expect(AlphabeticCurrencyCode.SEK).to.equal('SEK', 'AlphabeticCurrencyCode should contain "SEK"');
+  });
+  it('RSD', () => {
+    expect(AlphabeticCurrencyCode.RSD).to.equal('RSD', 'AlphabeticCurrencyCode should contain "RSD"');
+  });
+  it('BGN', () => {
+    expect(AlphabeticCurrencyCode.BGN).to.equal('BGN', 'AlphabeticCurrencyCode should contain "BGN"');
+  });
+  it('DOP', () => {
+    expect(AlphabeticCurrencyCode.DOP).to.equal('DOP', 'AlphabeticCurrencyCode should contain "DOP"');
+  });
+  it('KRW', () => {
+    expect(AlphabeticCurrencyCode.KRW).to.equal('KRW', 'AlphabeticCurrencyCode should contain "KRW"');
+  });
+  it('LVL', () => {
+    expect(AlphabeticCurrencyCode.LVL).to.equal('LVL', 'AlphabeticCurrencyCode should contain "LVL"');
+  });
+  it('VEF', () => {
+    expect(AlphabeticCurrencyCode.VEF).to.equal('VEF', 'AlphabeticCurrencyCode should contain "VEF"');
+  });
+  it('CZK', () => {
+    expect(AlphabeticCurrencyCode.CZK).to.equal('CZK', 'AlphabeticCurrencyCode should contain "CZK"');
+  });
+  it('TND', () => {
+    expect(AlphabeticCurrencyCode.TND).to.equal('TND', 'AlphabeticCurrencyCode should contain "TND"');
+  });
+  it('KWD', () => {
+    expect(AlphabeticCurrencyCode.KWD).to.equal('KWD', 'AlphabeticCurrencyCode should contain "KWD"');
+  });
+  it('VND', () => {
+    expect(AlphabeticCurrencyCode.VND).to.equal('VND', 'AlphabeticCurrencyCode should contain "VND"');
+  });
+  it('JOD', () => {
+    expect(AlphabeticCurrencyCode.JOD).to.equal('JOD', 'AlphabeticCurrencyCode should contain "JOD"');
+  });
+  it('NZD', () => {
+    expect(AlphabeticCurrencyCode.NZD).to.equal('NZD', 'AlphabeticCurrencyCode should contain "NZD"');
+  });
+  it('PAB', () => {
+    expect(AlphabeticCurrencyCode.PAB).to.equal('PAB', 'AlphabeticCurrencyCode should contain "PAB"');
+  });
+  it('CLP', () => {
+    expect(AlphabeticCurrencyCode.CLP).to.equal('CLP', 'AlphabeticCurrencyCode should contain "CLP"');
+  });
+  it('PEN', () => {
+    expect(AlphabeticCurrencyCode.PEN).to.equal('PEN', 'AlphabeticCurrencyCode should contain "PEN"');
+  });
+  it('GBP', () => {
+    expect(AlphabeticCurrencyCode.GBP).to.equal('GBP', 'AlphabeticCurrencyCode should contain "GBP"');
+  });
+  it('DZD', () => {
+    expect(AlphabeticCurrencyCode.DZD).to.equal('DZD', 'AlphabeticCurrencyCode should contain "DZD"');
+  });
+  it('CHF', () => {
+    expect(AlphabeticCurrencyCode.CHF).to.equal('CHF', 'AlphabeticCurrencyCode should contain "CHF"');
+  });
+  it('RUB', () => {
+    expect(AlphabeticCurrencyCode.RUB).to.equal('RUB', 'AlphabeticCurrencyCode should contain "RUB"');
+  });
+  it('UAH', () => {
+    expect(AlphabeticCurrencyCode.UAH).to.equal('UAH', 'AlphabeticCurrencyCode should contain "UAH"');
+  });
+  it('ARS', () => {
+    expect(AlphabeticCurrencyCode.ARS).to.equal('ARS', 'AlphabeticCurrencyCode should contain "ARS"');
+  });
+  it('SAR', () => {
+    expect(AlphabeticCurrencyCode.SAR).to.equal('SAR', 'AlphabeticCurrencyCode should contain "SAR"');
+  });
+  it('EGP', () => {
+    expect(AlphabeticCurrencyCode.EGP).to.equal('EGP', 'AlphabeticCurrencyCode should contain "EGP"');
+  });
+  it('INR', () => {
+    expect(AlphabeticCurrencyCode.INR).to.equal('INR', 'AlphabeticCurrencyCode should contain "INR"');
+  });
+  it('PYG', () => {
+    expect(AlphabeticCurrencyCode.PYG).to.equal('PYG', 'AlphabeticCurrencyCode should contain "PYG"');
+  });
+  it('TWD', () => {
+    expect(AlphabeticCurrencyCode.TWD).to.equal('TWD', 'AlphabeticCurrencyCode should contain "TWD"');
+  });
+  it('TRY', () => {
+    expect(AlphabeticCurrencyCode.TRY).to.equal('TRY', 'AlphabeticCurrencyCode should contain "TRY"');
+  });
+  it('BAM', () => {
+    expect(AlphabeticCurrencyCode.BAM).to.equal('BAM', 'AlphabeticCurrencyCode should contain "BAM"');
+  });
+  it('OMR', () => {
+    expect(AlphabeticCurrencyCode.OMR).to.equal('OMR', 'AlphabeticCurrencyCode should contain "OMR"');
+  });
+  it('SGD', () => {
+    expect(AlphabeticCurrencyCode.SGD).to.equal('SGD', 'AlphabeticCurrencyCode should contain "SGD"');
+  });
+  it('MAD', () => {
+    expect(AlphabeticCurrencyCode.MAD).to.equal('MAD', 'AlphabeticCurrencyCode should contain "MAD"');
+  });
+  it('BYR', () => {
+    expect(AlphabeticCurrencyCode.BYR).to.equal('BYR', 'AlphabeticCurrencyCode should contain "BYR"');
+  });
+  it('NIO', () => {
+    expect(AlphabeticCurrencyCode.NIO).to.equal('NIO', 'AlphabeticCurrencyCode should contain "NIO"');
+  });
+  it('HKD', () => {
+    expect(AlphabeticCurrencyCode.HKD).to.equal('HKD', 'AlphabeticCurrencyCode should contain "HKD"');
+  });
+  it('LTL', () => {
+    expect(AlphabeticCurrencyCode.LTL).to.equal('LTL', 'AlphabeticCurrencyCode should contain "LTL"');
+  });
+  it('SKK', () => {
+    expect(AlphabeticCurrencyCode.SKK).to.equal('SKK', 'AlphabeticCurrencyCode should contain "SKK"');
+  });
+  it('GTQ', () => {
+    expect(AlphabeticCurrencyCode.GTQ).to.equal('GTQ', 'AlphabeticCurrencyCode should contain "GTQ"');
+  });
+  it('BRL', () => {
+    expect(AlphabeticCurrencyCode.BRL).to.equal('BRL', 'AlphabeticCurrencyCode should contain "BRL"');
+  });
+  it('EUR', () => {
+    expect(AlphabeticCurrencyCode.EUR).to.equal('EUR', 'AlphabeticCurrencyCode should contain "EUR"');
+  });
+  it('HUF', () => {
+    expect(AlphabeticCurrencyCode.HUF).to.equal('HUF', 'AlphabeticCurrencyCode should contain "HUF"');
+  });
+  it('IQD', () => {
+    expect(AlphabeticCurrencyCode.IQD).to.equal('IQD', 'AlphabeticCurrencyCode should contain "IQD"');
+  });
+  it('CRC', () => {
+    expect(AlphabeticCurrencyCode.CRC).to.equal('CRC', 'AlphabeticCurrencyCode should contain "CRC"');
+  });
+  it('PHP', () => {
+    expect(AlphabeticCurrencyCode.PHP).to.equal('PHP', 'AlphabeticCurrencyCode should contain "PHP"');
+  });
+  it('SVC', () => {
+    expect(AlphabeticCurrencyCode.SVC).to.equal('SVC', 'AlphabeticCurrencyCode should contain "SVC"');
+  });
+  it('PLN', () => {
+    expect(AlphabeticCurrencyCode.PLN).to.equal('PLN', 'AlphabeticCurrencyCode should contain "PLN"');
+  });
+});
