f40cf6f6c1785ffd434b9038282d97cd298b652f
Craig A. Simko <csimko@channelape.com>
2019-05-02 10:06:16 -0400

    CA-4777 and CA-4778: Removing unused function definition / type files

--- src/orders/service/model/GetOrdersByRequestFunctionDefinition.ts
+++
@@ -1,7 +0,0 @@
-import { Order, OrdersQueryRequestByBusinessId, OrdersQueryRequestByChannel, OrdersQueryRequestByChannelOrderId }
-  from '../../..';
-
-type GetOrdersByRequestFunctionDefinition =
-  (ordersRequest: OrdersQueryRequestByBusinessId | OrdersQueryRequestByChannel |
-  OrdersQueryRequestByChannelOrderId, orders: Order[], deferred: Q.Deferred<any>, getSinglePage: boolean) => void;
-export default GetOrdersByRequestFunctionDefinition;
