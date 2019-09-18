c9b043a570063b5f2f3b93f6baaf4912c0a82988
Michael Bates <mbates@channelape.com>
2019-09-18 07:34:53 -0700

    CA-5251 Extracted promise mapping into service

--- src/users/service/UsersService.ts
+++ src/users/service/UsersService.ts
@@ -1,36 +1,25 @@
 import RequestClientWrapper from '../../RequestClientWrapper';
-import GenerateApiError from '../../utils/GenerateApiError';
 import * as Q from 'q';
 import Version from '../../model/Version';
 import Resource from '../../model/Resource';
 import User from '../model/User';
-import { AxiosResponse } from 'axios';
+import DalService from '../../DalService';
 
-export default class UsersService {
+export default class UsersService extends DalService {
 
   private readonly EXPECTED_GET_STATUS = 200;
 
-  constructor(private readonly client: RequestClientWrapper) { }
+  constructor(private readonly client: RequestClientWrapper) {
+    super();
+  }
 
   public get(userId: string): Promise<User> {
     const deferred = Q.defer<User>();
     const requestUrl = `/${Version.V1}${Resource.USERS}/${userId}`;
     this.client.get(requestUrl, {}, (error, response, body) => {
-      this.mapUserPromise(requestUrl, deferred, error, response, body, this.EXPECTED_GET_STATUS);
+      this.mapResponseToPromise(requestUrl, deferred, error, response, body, this.EXPECTED_GET_STATUS);
     });
     return deferred.promise as any;
-  }
-
-  private mapUserPromise(requestUrl: string, deferred: Q.Deferred<User>, error: any, response: AxiosResponse,
-    body: any, expectedStatusCode: number) {
-    if (error) {
-      deferred.reject(error);
-    } else if (response.status === expectedStatusCode) {
-      deferred.resolve(body);
-    } else {
-      const channelApeErrorResponse = GenerateApiError(requestUrl, response, body, this.EXPECTED_GET_STATUS);
-      deferred.reject(channelApeErrorResponse);
-    }
   }
 
 }
