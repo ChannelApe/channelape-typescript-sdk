cdfb48362c95d6ef8c2d81ddbe88eba0c311b86c
Michael Bates <mbates@channelape.com>
2021-09-13 10:07:41 -0700

    Fix NPM tag version

--- scripts/get-npm-tag.js
+++ scripts/get-npm-tag.js
@@ -6,28 +6,13 @@ function run() {
     const LATEST = 'latest';
     const currentBranch = process.argv[2];
 
-    if (
-        isPullRequestWithTargetBranchOf(MASTER) ||
-        isPushToBranch(MASTER, currentBranch)
-    ) {
+    if (isPushToBranch(MASTER, currentBranch)) {
         writeValue(LATEST);
         return;
     }
 
-    if (
-        isPullRequestWithTargetBranchOf(DEVELOP) ||
-        isPushToBranch(DEVELOP, currentBranch)
-    ) {
-        writeValue(DEVELOP);
-        return;
-    }
-
-    writeValue('');
+    writeValue(DEVELOP);
     return;
-}
-
-function isPullRequestWithTargetBranchOf(branchName) {
-    return process.env.GITHUB_BASE_REF && process.env.GITHUB_BASE_REF === branchName;
 }
 
 function isPushToBranch(expectedBranch, currentBranch) {
