38213a24a1765294e9f284be66d579b1221f0611
Michael Bates <mbates@channelape.com>
2021-09-13 08:37:43 -0700

    Fixed order refund issues and fixed npm tag value

---
+++ scripts/get-npm-tag.js
@@ -0,0 +1,40 @@
+run();
+
+function run() {
+    const DEVELOP = 'develop';
+    const MASTER = 'master';
+    const LATEST = 'latest';
+    const currentBranch = process.argv[2];
+
+    console.log(JSON.stringify(process.env, null, 4));
+    if (
+        isPullRequestWithTargetBranchOf(MASTER) ||
+        isPushToBranch(MASTER, currentBranch)
+    ) {
+        writeValue(LATEST);
+        return;
+    }
+
+    if (
+        isPullRequestWithTargetBranchOf(DEVELOP) ||
+        isPushToBranch(DEVELOP, currentBranch)
+    ) {
+        writeValue(DEVELOP);
+        return;
+    }
+
+    writeValue('');
+    return;
+}
+
+function isPullRequestWithTargetBranchOf(branchName) {
+    return process.env.GITHUB_BASE_REF && process.env.GITHUB_BASE_REF === branchName;
+}
+
+function isPushToBranch(expectedBranch, currentBranch) {
+    return !process.env.GITHUB_BASE_REF && currentBranch === expectedBranch;
+}
+
+function writeValue(value) {
+    process.stdout.write(value);
+}
