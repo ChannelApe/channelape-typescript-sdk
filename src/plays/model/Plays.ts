7f3812b4036c8c3660e5647ebca8b0e4eca3131c
ManojKumar <manoj.c@channel.com>
2022-01-03 13:15:04 +0530

    retrieve a list of all play definitions which are able to be installed

---
+++ src/plays/model/Plays.ts
@@ -0,0 +1,6 @@
+export default interface Plays {
+  createdAt: Date;
+  id: string;
+  name: string;
+  updatedAt: Date;
+}
--- src/plays/service/PlaysService.ts
+++ src/plays/service/PlaysService.ts
@@ -7,6 +7,7 @@ import Resource from '../../model/Resource';
 import Play from '../model/Play';
 import GenerateApiError from '../../utils/GenerateApiError';
 import StepsService from '../../steps/service/StepsService';
+import Plays from '../model/Plays';
 
 const EXPECTED_GET_STATUS = 200;
 
@@ -16,13 +17,22 @@ export default class PlaysService {
     private readonly stepsService: StepsService
   ) { }
 
-  public get(playId: string): Promise<Play> {
+  public get(playId?: string): Promise<Play | Plays[]> {
+    if (typeof(playId) === 'undefined') {
+      const deferred = Q.defer<Plays[]>();
+      const requestUrl = `/${Version.V2}${Resource.PLAYS}`;
+      this.client.get(requestUrl, {}, (error, response, body) => {
+        this.mapPlayPromises(requestUrl, deferred, error, response, body, EXPECTED_GET_STATUS);
+      });
+      return deferred.promise as any;
+    }
     const deferred = Q.defer<Play>();
     const requestUrl = `/${Version.V1}${Resource.PLAYS}/${playId}`;
     this.client.get(requestUrl, {}, (error, response, body) => {
       this.mapPlayPromise(requestUrl, deferred, error, response, body, EXPECTED_GET_STATUS);
     });
     return deferred.promise as any;
+
   }
   private mapPlayPromise(requestUrl: string, deferred: Q.Deferred<Play>, error: any, response: AxiosResponse,
     body: any, expectedStatusCode: number) {
@@ -36,7 +46,26 @@ export default class PlaysService {
       deferred.reject(playApeErrorResponse);
     }
   }
-
+  private mapPlayPromises(requestUrl: string, deferred: Q.Deferred<Plays[]>, error: any, response: AxiosResponse,
+    body: any, expectedStatusCode: number) {
+    if (error) {
+      deferred.reject(error);
+    } else if (response.status === expectedStatusCode) {
+      const plays: Plays [] = this.formatPlays(body.plays);
+      deferred.resolve(plays);
+    } else {
+      const playApeErrorResponse = GenerateApiError(requestUrl, response, body, EXPECTED_GET_STATUS);
+      deferred.reject(playApeErrorResponse);
+    }
+  }
+  private formatPlays(plays: Plays[]): Plays[] {
+    return plays.map((play:Plays) => this.formatAllPlay(play));
+  }
+  private formatAllPlay(play: Plays): Plays {
+    play.createdAt = new Date(play.createdAt);
+    play.updatedAt = new Date(play.updatedAt);
+    return play as Plays;
+  }
   private formatPlay(play: any): Play {
     play.createdAt = new Date(play.createdAt);
     play.updatedAt = new Date(play.updatedAt);
