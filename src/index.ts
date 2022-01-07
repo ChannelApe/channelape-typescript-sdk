ba1b8913a4a0101d4a7a75a90ad5c075df7c987b
Leela Apparao <leela.apparao@channelape.com>
2022-01-07 11:06:12 +0530

    Fix for quotes

--- src/index.ts
+++ src/index.ts
@@ -102,9 +102,9 @@ export { default as InvitationResponse } from './businesses/model/InvitationResp
 export { default as SupplierCreateRequest } from './suppliers/model/SupplierCreateRequest';
 export { default as PlaySettings } from './suppliers/model/PlaySettings';
 export { default as Play } from './plays/model/Play';
-export { default as PlaySchedule } from "./plays/model/PlaySchedule";
-export { default as PlayUpdateRequest } from "./plays/model/PlayUpdateRequest";
-export { default as PlaysService } from "./plays/service/PlaysService";
+export { default as PlaySchedule } from './plays/model/PlaySchedule';
+export { default as PlayUpdateRequest } from './plays/model/PlayUpdateRequest';
+export { default as PlaysService } from './plays/service/PlaysService';
 export { default as LocationClosureRequest } from './locations/model/LocationClosureRequest';
 export { default as LocationSLAUpdateRequest } from './locations/model/LocationSLAUpdateRequest';
 export { default as LocationSLAOperatingDayUpdateRequest }
