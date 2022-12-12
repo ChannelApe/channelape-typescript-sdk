593b31c6a6f3b437e99b96e8c9cad8ad0d42958a
Walter Kuhn <walt.kuhn@channelape.com>
2022-12-12 15:07:30 -0600

    lint

--- test/logging/service/LoggingService.spec.ts
+++ test/logging/service/LoggingService.spec.ts
@@ -1,18 +1,18 @@
-import { expect } from "chai";
-import * as sinon from "sinon";
-import LogLevel from "../../../src/model/LogLevel";
-import Version from "../../../src/model/Version";
-import Environment from "../../../src/model/Environment";
-import RequestClientWrapper from "../../../src/RequestClientWrapper";
-import LoggingService from "./../../../src/logging/service/LoggingService";
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import LogLevel from '../../../src/model/LogLevel';
+import Version from '../../../src/model/Version';
+import Environment from '../../../src/model/Environment';
+import RequestClientWrapper from '../../../src/RequestClientWrapper';
+import LoggingService from './../../../src/logging/service/LoggingService';
 
-describe("Logging Service", () => {
-  describe("Given some rest client", () => {
+describe('Logging Service', () => {
+  describe('Given some rest client', () => {
     const client: RequestClientWrapper = new RequestClientWrapper({
       endpoint: Environment.STAGING,
       maximumRequestRetryTimeout: 10000,
       timeout: 60000,
-      session: "valid-session-id",
+      session: 'valid-session-id',
       logLevel: LogLevel.INFO,
       minimumRequestRetryRandomDelay: 50,
       maximumRequestRetryRandomDelay: 50,
@@ -31,27 +31,27 @@ describe("Logging Service", () => {
       done();
     });
 
-    it("logs a payload", () => {
+    it('logs a payload', () => {
       const response = {
         status: 201,
         config: {
-          method: "POST",
+          method: 'POST',
         },
       };
 
       const payload = {
-        flow: "testflow",
-        body: "test message",
+        flow: 'testflow',
+        body: 'test message',
       };
 
       const expectedResult = {
-        flow: "testflow",
-        logtime: "2022-12-08T16:29:36.868Z",
-        businessId: "d2e9f8fa-926b-4690-ae08-c098e0220fad",
+        flow: 'testflow',
+        logtime: '2022-12-08T16:29:36.868Z',
+        businessId: 'd2e9f8fa-926b-4690-ae08-c098e0220fad',
       };
 
       const clientGetStub: sinon.SinonStub = sandbox
-        .stub(client, "post")
+        .stub(client, 'post')
         .yields(null, response, expectedResult);
 
       const loggingService: LoggingService = new LoggingService(client);
