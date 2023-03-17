7013ae37a215fff4ee2290f88c8bdb5b265b0f9f
Christopher Duda <cduda@cerebros.com>
2023-03-17 12:54:29 -0400

    fixed linting

--- src/RequestClientWrapper.ts
+++ src/RequestClientWrapper.ts
@@ -369,6 +369,7 @@ export default class RequestClientWrapper {
       const base64Url = token.split('.')[1];
       const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
       const jsonPayload = decodeURIComponent(atob(base64).split('').map((c) =>  {
+        // tslint:disable-next-line: prefer-template
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
       }).join(''));
       return JSON.parse(jsonPayload);
